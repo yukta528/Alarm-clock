592eb7061416e29f068470f3f8c0c2c18679fa5e
yukta528 <44142667+yukta528@users.noreply.github.com>
2023-07-20 23:24:42 +0530

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,29 @@
+This is a Digital Alarm Clock web application which user can use to set and manage multiple alarms. The application is built using HTML, CSS, and JavaScript.    
+
+Features 
+1-	Displaying current time (clock)
+2-	Set alarms by specifying the hours, minutes, seconds, and AM/PM.
+3-	Display the list of all alarms with each alarm having a "Delete" button.
+4-	Delete an alarm from the list of alarms using the “Delete” button.
+5-	Display an alert when alarm goes off (current time matches with one of the alarm).    
+
+How to Use 
+1-Clone the repository or download the source code to your local machine.  
+2-Open the index.html file in your web browser to access the Digital Alarm Clock application.    
+
+The Digital Alarm Clock UI has the below mentioned components:  
+1-Time Display: This section displays the current time in 12-hour format - (HH:MM:SS AM/PM). The time is automatically updates every second as time passes. 
+
+2-Time values input for alarm: User can enter values for hours, minutes and seconds for the alarm and select either AM or PM from the picklist and alarm gets added to the list when user clicks the “Set Alarm” button.
+
+If the user enters values outside range(for example- <0 or >12 or minutes/seconds <0 or >59), it will display an alert asking the user to enter correct values.
+
+If the user leaves out any value- hh, mm or ss, it will display an alert asking the user to enter all values.
+
+
+3-Alarm List: The list displays all the alarms set by user. Each list item shows the time in 12-hour format, and there is a "Delete" button for each alarm. Alarm gets deleted from the alarm list when user clicks the “Delete” button.
+
+If an alarm time matches the current time, you will receive an alert indicating that you have set an alarm with the matching time and alarm is going off now. 
+
+
+ Author This Digital Alarm Clock application is developed by Yukta Sharma. You can contact me at yuktasharma2000@gmail.com for any questions or inquiries related to this project.
---
+++ script.js
@@ -0,0 +1,100 @@
+//fetching and displaying current time
+let time = document.querySelector('#currTime');
+setInterval(function () {
+    let date_str = new Date().toLocaleTimeString();
+    if (date_str.length < 11) {
+        date_str = "0" + date_str;
+    }
+    time.innerHTML = date_str;
+}, 1000);
+
+
+//adding event listener for Alarm set
+
+let alarm_list = document.querySelector('#alarm_list');
+document.querySelector('#submit_alarm').addEventListener('click', addAlarm);
+
+//function to add alarm to list
+function addAlarm() {
+
+    //fetching input time values
+    let hours = document.querySelector('#hh');
+    let hours_val = hours.value;
+    let mins = document.querySelector('#mm')
+    let mins_val = mins.value;
+    let seconds = document.querySelector('#ss')
+    let sec_val = seconds.value;
+
+    //conditions if input type does not match standard criteria
+    if (hours_val == "" || mins_val == "" || sec_val == "") {
+        alert("Please enter all values to set an alarm");
+        return;
+    }
+    if (hours_val > 12 || hours_val < 0) {
+        alert("Please enter a value between 0 and 12 to set an alarm");
+        return;
+    }
+    if (mins_val > 59 || mins_val < 0) {
+        alert("Please enter a value between 0 and 59 to set an alarm");
+        return;
+    }
+    if (sec_val > 59 || sec_val < 0) {
+        alert("Please enter a value between 0 and 59 to set an alarm");
+        return;
+    }
+
+    let am_pm = document.querySelector('#ampm :checked').innerText;
+
+
+    //adding new alarm list element to list
+    let time_set = hours_val + ":" + mins_val + ":" + sec_val + " " + am_pm;
+    const li = document.createElement('li');
+    li.innerHTML = `
+    <label >${time_set}</label>
+    <button class="delete_alarm" type="submit">Delete</button>
+    `;
+
+    alarm_list.appendChild(li);
+
+    //resetting the input boxes to no value
+    hours.value = "";
+    mins.value = "";
+    seconds.value = "";
+
+    //fetching new alarm list and adding event listener to delete alarm
+    const list_all_alarms = [...document.querySelectorAll('#alarm_list li')];
+
+
+
+    for (const i of list_all_alarms) {
+
+        i.addEventListener('click', deleteAlarm);
+    }
+}
+
+
+
+
+//handler function to delete alarm from list
+function deleteAlarm(e) {
+    if (e.target.className == 'delete_alarm') {
+        this.parentNode.removeChild(this);
+    }
+}
+
+//check if any alarm goes off at current time
+setInterval(function () {
+    const alarm_check = [...document.querySelectorAll('#alarm_list li label')];
+    for (const j of alarm_check) {
+        let alarm_str = new Date().toLocaleTimeString();
+        if (alarm_str.length < 11) {
+            alarm_str = "0" + alarm_str;
+        }
+
+        let time_check = alarm_str;
+        if (j.innerText == time_check) {
+            alert("You set an alarm! It's " + time_check);
+
+        }
+    }
+}, 1000);
